41b6c35f9cdd14b7af1391d9b8ccfe592abb0182
Pablo <dominguezpg.99@gmail.com>
2025-01-27 22:21:51 -0300

    Parte Clase 03

---
+++ clase03 - react/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "../css/Header.css"
+
+const Header = () => {
+  return (
+    <div className="Header" id="header">
+          <h1>Clase de Props</h1>
+          
+    </div>
+  )
+}
+
+export default Header
